ad980cdf8e309c72f24519e184c26ac6f8feaf63
Biswajit Das <125547489+BiswajitDas13@users.noreply.github.com>
2025-10-04 12:19:22 +0530

    Update mc_client.py

--- mc_client.py
+++ mc_client.py
@@ -14,7 +14,7 @@ client = MultiServerMCPClient(
         "math": {
             "command": "python",
             # Make sure to update to the full absolute path to your math_server.py file
-            "args": ["mcp_server.py"],
+            "args": ["mc_server1.py"],
             "transport": "stdio",
         },
         "weather": {
