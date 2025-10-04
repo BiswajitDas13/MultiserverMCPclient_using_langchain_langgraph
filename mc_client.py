1c9658f8dc01e365c9771a209a149bcdd7b05995
biswajitdas13 <biswajitdascs13@gmail.com>
2025-10-04 12:13:12 +0530

    first commit

---
+++ mc_client.py
@@ -0,0 +1,52 @@
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langgraph.graph import StateGraph, MessagesState, START
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import asyncio
+
+load_dotenv()
+
+llm=ChatOpenAI(model="gpt-4o-mini")
+
+client = MultiServerMCPClient(
+    {
+        "math": {
+            "command": "python",
+            # Make sure to update to the full absolute path to your math_server.py file
+            "args": ["mcp_server.py"],
+            "transport": "stdio",
+        },
+        "weather": {
+            # make sure you start your weather server on port 8000
+            "url": "http://localhost:8000/mcp/",
+            "transport": "streamable_http",
+        }
+    }
+)
+async def main():
+    tools = await client.get_tools()
+
+    def call_model(state: MessagesState):
+        response = llm.bind_tools(tools).invoke(state["messages"])
+        return {"messages": response}
+
+    builder = StateGraph(MessagesState)
+    builder.add_node(call_model)
+    builder.add_node(ToolNode(tools))
+    builder.add_edge(START, "call_model")
+    builder.add_conditional_edges(
+        "call_model",
+        tools_condition,
+    )
+    builder.add_edge("tools", "call_model")
+    graph = builder.compile()
+    math_response = await graph.ainvoke({"messages": "what's (3 + 5) x 12?"})
+    weather_response = await graph.ainvoke({"messages": "what is the weather in nyc?"})
+
+    print("math response:", math_response["messages"][-1])
+    print("weather response:", weather_response["messages"][-1])
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
